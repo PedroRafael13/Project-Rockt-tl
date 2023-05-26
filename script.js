9ca786f28ffe12fefcea40bc36082256d33f3b99
PedroRafael13 <pedrorafaeldsn12@gmail.com>
2023-05-26 17:22:37 -0400

    primeiro comite

---
+++ script.js
@@ -0,0 +1,8 @@
+const mybutton = document.getElementById('button')
+
+function value(){
+  console.log('deu certo')
+  alert(`congratulations, you joined our team :) `)
+}
+
+mybutton.addEventListener("click", value)
